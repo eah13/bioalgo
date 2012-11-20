88ac2addc94dad5fba57a8f5d6f95e2062022748
Elliott Hauser <eah13@mac.com>
2012-11-20 11:18:00 -0500

    BWT transform code & extra example

--- hw4/bwtransform.py
+++ hw4/bwtransform.py
@@ -1,15 +1,36 @@
-def BWT(s, f):
-	rotation = [s[i:] + s[:i] for i in xrange(len(s))] 
-	# sort rows alphabetically
-	rotation.sort() 
-	# return (last column of the table)
-	bwt = "".join([r[-1] for r in rotation])
-	output = open(f, 'w')
-	output.write(bwt)
-	    
-print "This is a script to perform Burrows-Wheeler Transformations on strings."
-print "Be sure to include an end-of-string character ('$') at the end."
-string = raw_input("Enter string:")
-file = raw_input("Enter filename:")
 
+from sys import argv
+
+
+def BWT(s, f):
+		
+		rotation = [s[i:] + s[:i] for i in xrange(len(s))] 
+		# sort rows alphabetically
+		rotation.sort() 
+		# return (last column of the table)
+		bwt = "".join([r[-1] for r in rotation])
+		output = open(f, 'w')
+		output.write(bwt)
+
+def getargs():
+	global string
+	global file
+	if len(argv) == 3:
+		string = argv[1]
+		file = argv[2]
+	
+	elif len(argv) ==1:
+				
+		print "This is a script to perform Burrows-Wheeler Transformations on strings."
+		print "Be sure to include an end-of-string character ('$') at the end."
+		string = raw_input("Enter string:")
+		file = raw_input("Enter filename:")
+	
+	else:
+		print "Usage: <string> <file>"
+		return -1
+
+string = ""
+file = ""
+getargs()
 BWT(string, file)
