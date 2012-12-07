0c45efb3d8285bde85a9bd2313294859ee659aa9
Elliott Hauser <eah13@mac.com>
2012-12-06 23:06:41 -0500

    Complete except for Q1

--- hw5/distmat.py
+++ hw5/distmat.py
@@ -1,16 +1,18 @@
-
 A = [0,18,15,21,6,16]
-B = [18,0,20,19,20,24]
+B = [18,0,23,19,20,24]
 C = [15,23,0,26,17,19]
 D = [21,19,26,0,23,27]
 E = [6,20,17,23,0,18]
 F = [16,24,19,27,18,0]
 
+
+
 matrix = [A,B,C,D,E,F]
 
 
-
 def find_triples():
+	
+
 	matches = []
 	triples = 0
 
@@ -29,29 +31,28 @@ def find_triples():
 					matches.append([i,j,k])	
 					triples += 1
 
-	matches.sort()
-	for match in matches:
+	m = sorted(matches)
+	for match in m:
 	 	print matrix[match[0]][match[1]], matrix[match[1]][match[2]], matrix[match[0]][match[2]]
 
-	if len(matches) == triples:
-		print "All triples satisfy d(i,j) + d(j,k) >= d(i,k)"
-	else:
-		print "Not additive"
+
+find_triples()
 
 def four_point():
+# Determines whether a matrix is additive by testing for the four point condidtion.  Expects matrix as a list of lists
 
-	mismatch = []
+	n = len(matrix)
 
-	for i in xrange(0,5):
-		for j in xrange(0,5):
+	for i in xrange(0,n):
+		for j in xrange(0,n):
 			if i == j:
 				continue
-			for k in xrange(0,5):
+			for k in xrange(0,n):
 				if k == j:
 					continue
 				if k == i:
 					continue
-				for l in xrange(0,5):
+				for l in xrange(0,n):
 					if l == k:
 						continue
 					if l == j:
@@ -61,15 +62,26 @@ def four_point():
 					one = matrix[i][j] + matrix[k][l]
 					two = matrix[i][k] + matrix[j][l]
 					three = matrix[i][l] + matrix[j][k]
-					lump = [one,two,three]
-					together = set(lump)
-					if len(together) != 2:
-						print i,j,k,l
-						print lump
-						print matrix[i][j]," + ",matrix[k][l]," = ",one
-						print matrix[i][k]," + ",matrix[j][l]," = ",two
-						print matrix[i][l]," + ",matrix[j][k]," = ",three
-						print together
+					lump = sorted([one,two,three])
+					if lump[1] != lump[2]:
+						print "Not additive"
 
 
-four_point()
+def sankoff():
+
+	chimp = ['G','T','T','G']
+	gorilla = ['G','T','C','A']
+	human = ['A','C','C','A']
+	orangutan = ['A','T','T','A']
+
+	A = [0,2,3,8]
+	T = [2,0,1,3]
+	C = [3,1,0,3]
+	G = [8,3,3,0]
+
+	matrix = [A,T,C,G]
+
+
+
+
+
